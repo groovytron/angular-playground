86a34af8d2534e0c0024672ec085efe7c1f0f99c
Julien M'Poy <julien.mpoy@gmail.com>
2021-10-22 23:57:03 +0200

    Add header component and fontawesome to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-  ],
+  imports: [CoreModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,29 @@
+import { AppRoutingModule } from '../app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [HeaderComponent],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    HttpClientModule,
+    MatButtonModule,
+  ],
+  exports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    HttpClientModule,
+    HeaderComponent,
+  ],
 })
 export class CoreModule {}
